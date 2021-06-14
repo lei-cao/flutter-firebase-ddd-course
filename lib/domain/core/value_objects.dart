417a64bf73c7562f3cfd06c79b1bf66386414dfd
Lei Cao <lei@remerge.io>
2021-06-14 16:21:21 +0800

    part 13: Note Entities

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -7,9 +7,17 @@ import 'failures.dart';
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
+
   Either<ValueFailure<T>, T> get value;
 
   T getOrCrash() => value.fold((f) => throw UnexpectedValueError(f), id);
+
+  Either<ValueFailure<dynamic>, Unit> get failureOrUnit {
+    return value.fold(
+      (l) => left(l),
+      (r) => right(unit),
+    );
+  }
 
   bool isValid() => value.isRight();
 
@@ -27,22 +35,21 @@ abstract class ValueObject<T> {
   int get hashCode => value.hashCode;
 }
 
-
 class UniqueId extends ValueObject<String> {
-    @override
-    final Either<ValueFailure<String>, String> value;
+  @override
+  final Either<ValueFailure<String>, String> value;
 
-    factory UniqueId() {
-        return UniqueId._(
-          right(const Uuid().v1()),
-        );
-    }
+  factory UniqueId() {
+    return UniqueId._(
+      right(const Uuid().v1()),
+    );
+  }
 
-    factory UniqueId.fromUniqueString(String uniqueId) {
-      return UniqueId._(
-        right(uniqueId),
-      );
-    }
+  factory UniqueId.fromUniqueString(String uniqueId) {
+    return UniqueId._(
+      right(uniqueId),
+    );
+  }
 
-    const UniqueId._(this.value);
+  const UniqueId._(this.value);
 }
